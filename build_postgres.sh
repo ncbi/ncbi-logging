54e0b61578e13c9eff88d0ebe7f1f05b42aba738
Mike Vartanian <Mike.Vartanian@nih.gov>
2019-07-30 13:23:38 -0400

    Higher optimization

--- build_postgres.sh
+++ build_postgres.sh
@@ -8,8 +8,9 @@ curl -O "https://ftp.postgresql.org/pub/source/v11.4/$PGVER.tar.bz2"
 tar -xaf "$PGVER.tar.bz2"
 rm -f "$PGVER.tar.bz2"
 cd $PGVER || exit
+export CFLAGS="-march=native -mavx -O3"
 ./configure "--prefix=$PWD"
-nice -n 19 make -j 32
+make -j 32
 make check && make install
 cd ..
 tar -caf "$PGVER.rel.tar.gz" "$PGVER/bin" "$PGVER/share" "$PGVER/lib"
