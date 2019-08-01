bf309e37f3f95fa3213152689c1ff3005db7ac36
Mike Vartanian <Mike.Vartanian@nih.gov>
2019-08-01 08:36:33 -0400

    Update cron timings

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@ rm -f ./s3tojson ./nginxtojson
 
 g++ --std=c++17 -O3 -march=native \
           -Wall -Wextra -Wpedantic \
+          -flto \
           -o s3tojson \
           s3tojson.cpp
 
 g++ --std=c++17 -O3 -march=native \
           -Wall -Wextra -Wpedantic \
+          -flto \
           -o nginxtojson \
           nginxtojson.cpp
 #          -fsanitize=address \
