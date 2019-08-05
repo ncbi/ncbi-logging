fcbc5a5a0ac74586e068b3a71a4ab8f793e4f88b
Mike Vartanian <Mike.Vartanian@nih.gov>
2019-08-05 09:17:23 -0400

    Add counts

--- import.sh
+++ import.sh
@@ -38,58 +38,66 @@ time psql -h localhost -d grafana << HERE
     CREATE index export_source on export (source);
     CLUSTER export using export_start;
 */
-    select count(*) from export;
+select count(*) AS export_count FROM export;
 
 
-    CREATE TEMP TABLE ips_export as select distinct ip, ip_int from export;
+CREATE TEMP TABLE ips_export AS select distinct ip, ip_int FROM export;
 
-    DROP TABLE IF EXISTS ips_export2;
-    CREATE TABLE ips_export2 as
-        select ips_export.ip, ip_int, domain, city_name, country_code
-        from ips_export, ip2location_db11_ipv4, rdns
-        WHERE
-            BOX(POINT(ip_from,ip_from),POINT(ip_to,ip_to)) @>
-            BOX(POINT (ip_int,ip_int), POINT(ip_int,ip_int))
-            AND rdns.ip=ips_export.ip ;
-    CREATE INDEX ips_export2_ip on ips_export2(ip);
-    CREATE INDEX ips_export2_ipint on ips_export2(ip_int);
+DROP TABLE IF EXISTS ips_export2;
+CREATE TABLE ips_export2 as
+    select ips_export.ip, ip_int, domain, city_name, country_code
+    FROM ips_export, ip2location_db11_ipv4, rdns
+    WHERE
+        BOX(POINT(ip_FROM,ip_FROM),POINT(ip_to,ip_to)) @>
+        BOX(POINT (ip_int,ip_int), POINT(ip_int,ip_int))
+        AND rdns.ip=ips_export.ip ;
+
+SELECT count(*) AS ips_export2_clount FROM ips_export2;
+
+CREATE INDEX ips_export2_ip on ips_export2(ip);
+CREATE INDEX ips_export2_ipint on ips_export2(ip_int);
 
 
-    DROP TABLE IF EXISTS export_joined;
-    CREATE TABLE export_joined as
-        SELECT status, cmds, bytecount,
-            agent, cnt, acc, start_ts, end_ts, source,
-            host,
-            city_name, country_code, domain,
-            export.ip, export.ip_int
-        FROM export, ips_export2
-        WHERE
-            export.ip_int=ips_export2.ip_int
-        ORDER BY source, start_ts;
+DROP TABLE IF EXISTS export_joined;
+CREATE TABLE export_joined as
+    SELECT status, cmds, bytecount,
+        agent, cnt, acc, start_ts, end_ts, source,
+        host,
+        city_name, country_code, domain,
+        export.ip, export.ip_int
+    FROM export, ips_export2
+    WHERE
+        export.ip_int=ips_export2.ip_int
+    ORDER BY source, start_ts;
+
+SELECT count(*) AS export_joined_count FROM EXPORT_JOINED;
+
 BEGIN;
     ALTER TABLE cloud_sessions RENAME TO cloud_sessions_bak;
     ALTER TABLE export_joined RENAME TO cloud_sessions;
 COMMIT;
+
+SELECT count(*) AS cloud_sessions_count FROM cloud_sessions;
 
     DROP index cloud_sessions_start;
     DROP index cloud_sessions_source;
     CREATE index cloud_sessions_start on cloud_sessions (start_ts);
     CREATE index cloud_sessions_source on cloud_sessions (source);
 
--- Materialized views, as tables in case we need to index them
+-- Materialized views, AS tables in case we need to index them
 BEGIN;
     DROP TABLE IF EXISTS last_used;
     CREATE TABLE last_used as
-        select acc, max(last) as last
-        from  (
-            select acc, date_trunc('day', start_ts) as last
-            from cloud_sessions
+        select acc, max(last) AS last
+        FROM  (
+            select acc, date_trunc('day', start_ts) AS last
+            FROM cloud_sessions
             where source='SRA'
             and acc ~ '[DES]RR[\d\.]{6,10}'
             union all
-            select run as acc, '2018-12-01 00:00:00'::timestamp as last
-            from public
-        ) as roll2
+            select run AS acc, '2018-12-01 00:00:00'::timestamp AS last
+            FROM public
+        ) AS roll2
         group by acc;
 COMMIT;
 
@@ -102,18 +110,18 @@ BEGIN;
         when date_trunc('day', age(localtimestamp, last)) > interval '180 days'
         then 'never downloaded'
         else 'downloaded 30..180 days ago'
-    end as metric,
-    count(*) as value
-    from last_used
+    end AS metric,
+    count(*) AS value
+    FROM last_used
     group by metric;
 COMMIT;
 
 BEGIN;
     DROP TABLE IF EXISTS downloads_by_ip;
     CREATE TABLE downloads_by_ip as
-        SELECT date_trunc('day', start_ts) as time,
+        SELECT date_trunc('day', start_ts) AS time,
         source,
-        count(distinct ip) as unique_users
+        count(distinct ip) AS unique_users
         FROM
         cloud_sessions
         WHERE
@@ -132,9 +140,9 @@ BEGIN;
         when date_trunc('day', age(localtimestamp, last)) > interval '180 days'
         then 'never downloaded'
         else 'downloaded 30..180 days ago'
-    end as metric,
-    count(*) as value
-    from last_used
+    end AS metric,
+    count(*) AS value
+    FROM last_used
     group by metric;
 COMMIT;
 
@@ -142,9 +150,9 @@ BEGIN;
     DROP TABLE IF EXISTS daily_downloads;
     CREATE TABLE daily_downloads as
     SELECT
-        date_trunc('day', start_ts) as time,
-        sum(bytecount) as bytes,
-        sum(cnt) as downloads
+        date_trunc('day', start_ts) AS time,
+        sum(bytecount) AS bytes,
+        sum(cnt) AS downloads
     FROM
     cloud_sessions
     WHERE
@@ -160,10 +168,10 @@ BEGIN;
     select source || ' -> ' || domain
     || ' @ ' || city_name
     || ', ' || country_code
-    as Path,
-    sum(cnt) as Downloads, sum(bytecount) as Bytes,
-    date_trunc('day', start_ts) as time
-    from cloud_sessions
+    AS Path,
+    sum(cnt) AS Downloads, sum(bytecount) AS Bytes,
+    date_trunc('day', start_ts) AS time
+    FROM cloud_sessions
     where
     ( cmds like '%GET%' or cmds like '%HEAD%' )
     AND source!='SRA'
@@ -175,19 +183,19 @@ BEGIN;
     DROP TABLE IF EXISTS download_domains;
     CREATE TABLE download_domains as
         select domain,
-        date_trunc('day', start_ts) as time,
-        sum(bytes) as Bytes
-        from sra_cloud
+        date_trunc('day', start_ts) AS time,
+        sum(bytes) AS Bytes
+        FROM sra_cloud
         group by domain, time;
 COMMIT;
 
 BEGIN;
     DROP TABLE IF EXISTS cloud_organisms;
     CREATE TABLE cloud_organisms as
-    select initcap(acc_desc) as Organism,
-    count(*) as popularity,
-    date_trunc('day', start_ts) as time
-    from cloud_sessions, accs
+    select initcap(acc_desc) AS Organism,
+    count(*) AS popularity,
+    date_trunc('day', start_ts) AS time
+    FROM cloud_sessions, accs
     where
     ( cmds like '%GET%' or cmds like '%HEAD%' )
     AND cloud_sessions.acc=accs.acc
@@ -199,10 +207,10 @@ COMMIT;
 BEGIN;
     DROP TABLE IF EXISTS acc_last_acc;
     CREATE TABLE acc_last_acc as
-    select last as time, count(*) as accessions from (
-    select max(last) as last
-    from sra_last_used
-    group by acc) as roll
+    select last AS time, count(*) AS accessions FROM (
+    select max(last) AS last
+    FROM sra_last_used
+    group by acc) AS roll
     group by last
     order by last;
 COMMIT;
@@ -211,9 +219,9 @@ BEGIN;
     DROP TABLE IF EXISTS sra_cloud_downloads;
     CREATE TABLE sra_cloud_downloads as
     SELECT
-        date_trunc('day', start_ts) as time,
-        sum(bytes) as bytes,
-        sum(cnt) as downloads
+        date_trunc('day', start_ts) AS time,
+        sum(bytes) AS bytes,
+        sum(cnt) AS downloads
     FROM
         sra_cloud
     GROUP BY "time";
