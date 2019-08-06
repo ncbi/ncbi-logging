90b04147b8ca4124145d1613969ace907f6d581f
Mike Vartanian <Mike.Vartanian@nih.gov>
2019-08-06 09:17:47 -0400

    Add tests, Profile Guided Optimziation

--- build.sh
+++ build.sh
@@ -5,14 +5,49 @@ rm -f ./s3tojson ./nginxtojson
 g++ --std=c++17 -O3 -march=native \
  -Wall -Wextra -Wpedantic \
  -flto \
+ -fprofile-generate \
  -o s3tojson \
  s3tojson.cpp
 
 g++ --std=c++17 -O3 -march=native \
  -Wall -Wextra -Wpedantic \
  -flto \
+ -fprofile-generate \
  -o nginxtojson \
  nginxtojson.cpp
+
+gunzip -d -c /tmp/mike_logs/s3_prod/20190731.combine.gz | head -n 10000 | \
+    ./s3tojson > s3test.result 2>&1
+
+gunzip -d -c /tmp/mike_logs/gs_prod/20190731/sra-pub-src-1_usage_2019_07_31_22_00_00_00929704ade74e4bb8_v0.gz | head -n 10000 | \
+    ./nginxtojson > nginxtest.result 2>&1
+
+if ! diff nginxtest.result nginxtest.expected
+then
+    echo "nginx diff failed"
+fi
+
+if ! diff s3test.result s3test.expected
+then
+    echo "s3 diff failed"
+fi
+
+g++ --std=c++17 -O3 -march=native \
+ -Wall -Wextra -Wpedantic \
+ -flto \
+ -fprofile-use \
+ -o s3tojson \
+ s3tojson.cpp
+
+g++ --std=c++17 -O3 -march=native \
+ -Wall -Wextra -Wpedantic \
+ -flto \
+ -fprofile-use \
+ -o nginxtojson \
+ nginxtojson.cpp
+
+
+
 # -fsanitize=address \
 exit 0
 
