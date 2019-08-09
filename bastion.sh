6a70fb19e81a28fca1853c8b3bcc0bca5deaf58e
Mike Vartanian <Mike.Vartanian@nih.gov>
2019-08-09 13:45:41 -0400

    Runnable from cron

--- bastion.sh
+++ bastion.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 date
+SSH_ENV="$HOME/.ssh/environment.$HOSTNAME"
+. "$SSH_ENV"
+
 if ! lsof -ti:5433
 then
     echo "Restarting tunnel"
-    ssh -vx \
+    ssh -nvx \
     -L 5433:strides-analytics-db.cluster-cyen7j2us3rv.us-east-1.rds.amazonaws.com:5432 \
     vartanianmh@174.129.69.52 \
     'sleep 3600' &
