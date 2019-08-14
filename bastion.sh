826f40a30d8a3afd04fe82d788048b20fe302564
Mike Vartanian <Mike.Vartanian@nih.gov>
2019-08-14 07:52:27 -0400

    Prevent multiple ssh

--- bastion.sh
+++ bastion.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-echo "Starting tunnel"
+date
+SSH_ENV="$HOME/.ssh/environment.$HOSTNAME"
+. "$SSH_ENV"
 
-while true; do
-    date
-    SSH_ENV="$HOME/.ssh/environment.$HOSTNAME"
-    . "$SSH_ENV"
-
+if ! lsof -ti:5433
+then
     echo "Restarting tunnel"
     ssh -nvx \
     -L 5433:strides-analytics-db.cluster-cyen7j2us3rv.us-east-1.rds.amazonaws.com:5432 \
     vartanianmh@174.129.69.52 \
-    'sleep 3600'
-done
+    'sleep 3590'
+fi
