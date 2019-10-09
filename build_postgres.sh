5f78b5c0cfbe6d712624ab52faa844830c1b8d16
Mike Vartanian <Mike.Vartanian@nih.gov>
2019-10-09 12:40:34 -0400

    Build Postgres 12

--- build_postgres.sh
+++ build_postgres.sh
@@ -4,7 +4,7 @@
 . "$HOME/strides/strides_env.sh"
 
 rm -rf "$PGVER" "$PGVER.tar.bz2" "$PGVER.rel.tar.gz"
-curl -O "https://ftp.postgresql.org/pub/source/v11.5/$PGVER.tar.bz2"
+curl -O "https://ftp.postgresql.org/pub/source/v12.0/$PGVER.tar.bz2"
 tar -xaf "$PGVER.tar.bz2"
 rm -f "$PGVER.tar.bz2"
 cd $PGVER || exit
