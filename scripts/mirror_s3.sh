19b28ad1d3e95e8557ad11a874c3777e96161080
Mike Vartanian <Mike.Vartanian@nih.gov>
2020-06-06 15:08:12 -0400

    Add profile support

--- scripts/mirror_s3.sh
+++ scripts/mirror_s3.sh
@@ -5,12 +5,13 @@
 
 type -a aws
 
-buckets=$(sqlcmd "select distinct log_bucket from buckets where cloud_provider='S3' order by log_bucket desc")
+buckets=$(sqlcmd "select distinct log_bucket from buckets where cloud_provider='S3' order by log_bucket")
 
 #YESTERDAY="20200530"
 #YESTERDAY_DASY="2020-05-30"
 
 echo "buckets is '$buckets'"
+
 for LOG_BUCKET in $buckets; do
     # NOTES:
     # * aws s3 ls is much faster than other methods on buckets with
@@ -21,13 +22,23 @@ for LOG_BUCKET in $buckets; do
     mkdir -p "$DEST"
     cd "$DEST" || exit
 
+# TODO: Fetch scope for destination bucket
 # TODO: Fetch service account for AWS --profile
+    if [[ $LOG_BUCKET =~ "-pub-" ]]; then
+        DEST_BUCKET="gs://strides_analytics_logs_s3_public"
+        PROFILE="strides-analytics"
+    fi
+    if [[ $LOG_BUCKET =~ "-ca-" ]]; then
+        DEST_BUCKET="gs://strides_analytics_logs_s3_ca"
+        PROFILE="s3_readers"
+    fi
+
     # NOTE: Much faster to use aws ls than aws cp with include/excludes
     set +e
-    objects=$(aws s3 ls "s3://$LOG_BUCKET/$YESTERDAY_DASH" | cut -c 32- )
+    objects=$(aws s3 ls "s3://$LOG_BUCKET/$YESTERDAY_DASH" --profile "$PROFILE" | cut -c 32- )
     wc=$(echo "$objects" | wc -l)
     echo "Copying $wc objects from $LOG_BUCKET into $DEST"
-    echo "$objects" | xargs -I % -P 24 aws s3 cp "s3://$LOG_BUCKET/%" "$DEST/%" --quiet
+    echo "$objects" | xargs -I % -P 24 aws s3 --profile "$PROFILE" cp "s3://$LOG_BUCKET/%" "$DEST/%" --quiet
     set -e
     date
     echo "Copied $LOG_BUCKET to $DEST"
@@ -43,14 +54,6 @@ for LOG_BUCKET in $buckets; do
     export GOOGLE_APPLICATION_CREDENTIALS=$HOME/sandbox-blast-847af7ab431a.json
     gcloud config set account 1008590670571-compute@developer.gserviceaccount.com
     export CLOUDSDK_CORE_PROJECT="ncbi-sandbox-blast"
-
-# TODO: Fetch scope for destination bucket
-    if [[ $LOG_BUCKET =~ "-pub-" ]]; then
-        DEST_BUCKET="gs://strides_analytics_logs_s3_public"
-    fi
-    if [[ $LOG_BUCKET =~ "-ca-" ]]; then
-        DEST_BUCKET="gs://strides_analytics_logs_s3_ca"
-    fi
 
     gsutil cp "$TAR" "$DEST_BUCKET"
     gsutil ls -l "$DEST_BUCKET"
