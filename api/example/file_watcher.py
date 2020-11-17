58c4d2e8c688bcc30b1135a5a42683ed703ddd66
aboshkin <boshkina@ncbi.nlm.nih.gov>
2020-11-17 16:53:04 -0500

    added code to file_watcher.py

--- api/example/file_watcher.py
+++ api/example/file_watcher.py
@@ -1,20 +1,67 @@
 #! /usr/bin/env python3
 
-import argparse, sys
+import argparse, sys, os, glob, logging
+from urllib.parse import urlparse, urlunparse
 
-def perform_file_watching( location : str, concentrator_url : str, client ) -> bool :
-    if None == location :
+# False if there was at least one error opening or transmitting
+#   (some files may have been processed)
+# True if all files have been processed or there are none
+def perform_file_watching( basename : str, concentrator_url : str, client ) -> bool :
+    if None == basename :
         return False
     if None == concentrator_url :
         return False
     if None == client :
         return False
-    return True
+
+    if not os.path.isdir( os.path.dirname( basename ) ):
+        return False
+
+    o = urlparse( concentrator_url )
+    success = True
+    conn = None
+    files = glob.glob( basename + "*" )
+    for fn in files :
+        if fn != basename :
+            try:
+                if conn == None :
+                    conn = client.HTTPConnection( urlunparse( (o.scheme, o.netloc, "", "", "", "" ) ) )
+            except Exception as e:
+                # where this log message goes is up to the environment
+                logging.error( "perform_file_watching client.HTTPConnection failed (%s)", str(e) )
+                return False
+
+            try:
+
+                with open( fn ) as f:
+                    try:
+                        conn.request('POST', o.path, f.readlines() )
+                    except Exception as e:
+                        logging.error( "perform_file_watching conn.request failed (%s)", str(e) )
+                        conn.close()
+                        conn = None # will reopen on the next iteration
+                        success = False
+
+                    if conn != None :
+                        resp = conn.getresponse()
+                        if 200 == resp.status:
+                            os.remove( fn )
+                        else:
+                            success = False
+
+            except Exception as e:
+                logging.error( "perform_file_watching open(%s) failed (%s)", fn, str(e) )
+                success = False
+
+    if conn != None :
+        conn.close()
+
+    return success
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser( description = "file watcher" )
-    parser.add_argument( '--loc', '-l', metavar='path', type=str,
-        dest='location', default = None, help='directory to watch' )
+    parser.add_argument( '--pattern', '-p', metavar='path', type=str,
+        dest='location', default = None, help='files to watch' )
     parser.add_argument( '--url', '-u', metavar='url', type=str,
         dest='url', default = None, help='url to send logs to' )
     args = parser.parse_args()
--- api/example/test_file_watcher.py
+++ api/example/test_file_watcher.py
@@ -1,19 +1,157 @@
 #! /usr/bin/env python3
 
-import unittest, http.client
+import unittest, unittest.mock
+import http.client, os, shutil, logging
 import file_watcher
+
+from unittest.mock import Mock
+
+def createFile( filename : str, content : str ) -> str :
+    with open( filename, "w") as f:
+        f.write( content )
+    return filename
+
+def makeMockClient( status : int = 200 ) :
+    connection = Mock()
+    client = Mock()
+    client.HTTPConnection = Mock( return_value = connection )
+    connection.request = Mock()
+    response = Mock()
+    response.status = status
+    connection.getresponse = Mock( return_value = response )
+    return connection, client
+
+class Nothing:
+    def __init__ ( self ):
+        pass
 
 class Test_file_watching( unittest.TestCase ) :
     def setUp( self ) :
-        pass
+        self.location = os.path.join( os.getcwd(), 'tmp', unittest.TestCase.id( self ) )
+        shutil.rmtree( self.location, ignore_errors=True )
+        os.makedirs( self.location, exist_ok=True )
+        self.log = os.path.join( self.location, "log" )
+
+        self.logger = logging.getLogger()
+        self.level = self.logger.getEffectiveLevel()
 
     def tearDown( self ) :
-        pass
+        self.logger.setLevel(self.level)
+        shutil.rmtree( self.location, ignore_errors=True )
 
     def test_one( self ) :
         self.assertFalse( file_watcher.perform_file_watching( None, 'somewhere', http.client ) )
         self.assertFalse( file_watcher.perform_file_watching( 'somewhere', None, http.client ) )
         self.assertFalse( file_watcher.perform_file_watching( 'somewhere', 'somewhere', None ) )
+
+    def test_bad_location( self ) :
+        # the full basename does not have to exist, but the directory part of it should
+        self.assertFalse( file_watcher.perform_file_watching( '/i do not exist/basename', 'somewhere', http.client ) )
+
+    def test_empty_dir( self ) :
+        # 'Nothing' would blow up if called in any way
+        self.assertTrue( file_watcher.perform_file_watching( self.log, 'somewhere', Nothing ) )
+
+    def test_unfinished_log( self ) :
+        # log withtout timestamp should not be touched
+        with open( self.log,"w") as f:
+            f.write("blah")
+        self.assertTrue( file_watcher.perform_file_watching( self.log, 'somewhere', Nothing ) )
+
+    def test_transmit( self ) :
+        filename = createFile( self.log + ".suffix", "blah" )
+
+        connection, client = makeMockClient()
+
+        self.assertTrue( file_watcher.perform_file_watching( self.log, 'http://a.b.c:8899/endpoint/dir/file?k1=v1&k2=v2', client ) )
+
+        self.assertTrue( client.HTTPConnection.called )
+        self.assertEqual( 1, client.HTTPConnection.call_count )
+        self.assertEqual( ('http://a.b.c:8899',), client.HTTPConnection.call_args[0] )
+
+        self.assertEqual( 1, connection.request.call_count )
+        self.assertEqual( 'POST', connection.request.call_args[0][0] )
+        self.assertEqual( '/endpoint/dir/file', connection.request.call_args[0][1] )
+        self.assertEqual( "blah", connection.request.call_args[0][2][0] )
+
+        self.assertFalse( os.path.exists(filename) )
+
+    def test_HTTPConnection_fails( self ) :
+        createFile( self.log + ".suffix", "blah" )
+
+        # temporarily block ERROR messages from going to stderr
+        self.logger.setLevel( logging.CRITICAL )
+
+        self.assertFalse( file_watcher.perform_file_watching( self.log, 'http://a.b.c:8899', http.client ) )
+
+    def test_file_open_fails( self ) :
+        filename = createFile( self.log + ".suffix", "blah" )
+        os.chmod(filename, 2)
+
+        _, client = makeMockClient()
+
+        # temporarily block ERROR messages from going to stderr
+        self.logger.setLevel( logging.CRITICAL )
+
+        self.assertFalse( file_watcher.perform_file_watching( self.log, 'http://a.b.c:8899', client ) )
+
+    def test_file_open_fails_for_one_of_many( self ) :
+        filename1 = createFile( self.log + ".1", "blah1" )
+        filename2 = createFile( self.log + ".2", "blah2" )
+        filename3 = createFile( self.log + ".3", "blah3" )
+        # one of the 3 files will not open
+        os.chmod(filename2, 2)
+
+        _, client = makeMockClient()
+
+        # temporarily block ERROR messages from going to stderr
+        self.logger.setLevel( logging.CRITICAL )
+
+        self.assertFalse( file_watcher.perform_file_watching( self.log, 'http://a.b.c:8899', client ) )
+
+        # files 1 and 3 have been processed and removed, the 2 remains on disk
+        self.assertFalse( os.path.exists( filename1 ) )
+        self.assertTrue(  os.path.exists( filename2 ) )
+        self.assertFalse( os.path.exists( filename3 ) )
+
+    def test_request_fails( self ) :
+        filename = createFile( self.log + ".suffix", "blah" )
+
+        _, client = makeMockClient( 500 )
+
+        self.assertFalse( file_watcher.perform_file_watching( self.log, 'http://a.b.c:8899/endpoint/dir/file?k1=v1&k2=v2', client ) )
+
+        self.assertTrue( os.path.exists(filename) )
+
+    # def test_request_fails_for_one_of_many( self ) :
+    #     filename1 = createFile( self.log + ".1", "blah1" )
+    #     filename2 = createFile( self.log + ".2", "blah2" )
+    #     filename3 = createFile( self.log + ".3", "blah3" )
+    #     # one of the 3 files will not open
+    #     os.chmod(filename2, 2)
+
+    #     connection = Mock()
+    #     client = Mock()
+    #     client.HTTPConnection = Mock( return_value = connection )
+    #     connection.request = Mock()
+    #     response = Mock()
+    #     response.status = (200, 500, 200) ... or something
+    #     connection.getresponse = Mock( return_value = response )
+
+    #     # temporarily block ERROR messages from going to stderr
+    #     self.logger.setLevel( logging.CRITICAL )
+
+    #     self.assertFalse( file_watcher.perform_file_watching( self.log, 'http://a.b.c:8899', client ) )
+
+    #     # files 1 and 3 have been processed and removed, the 2 remains on disk
+    #     self.assertFalse( os.path.exists( filename1 ) )
+    #     self.assertTrue(  os.path.exists( filename2 ) )
+    #     self.assertFalse( os.path.exists( filename3 ) )
+
+
+#TODO: conn.request throws
+
+
 
 if __name__ == "__main__":
     unittest.main() # run all tests
