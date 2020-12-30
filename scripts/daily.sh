6c006b96367897aad60134973f10c26781396e09
Mike Vartanian <Mike.Vartanian@nih.gov>
2020-12-30 09:48:24 -0500

    Remove redundant timestamps

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -13,42 +13,42 @@ mkdir -p "$HOME"/logs
 
 /opt/panfs/bin/pan_df -H /panfs/traces01.be-md.ncbi.nlm.nih.gov/strides-analytics/
 
-date; echo "mirror.sh GS"
+echo "mirror.sh GS"
 ./mirror.sh GS |& ts >> "$HOME"/mirror_gs.log
 
-#date; echo "mirror.sh OP"
+#echo "mirror.sh OP"
 #./mirror.sh OP |& ts >> "$HOME"/mirror_op.log
 
-date; echo "mirror.sh S3"
+echo "mirror.sh S3"
 ./mirror.sh S3 |& ts >> "$HOME"/mirror_s3.log
 
-date; echo "mirror.sh Splunk"
+echo "mirror.sh Splunk"
 ./mirror.sh Splunk |& ts >> "$HOME"/mirror_splunk.log
 
 
-date; echo "parse S3"
+echo "parse S3"
 ./parse.sh S3      |& ts >> "$HOME"/parse_s3.log
 ./parse_new.sh S3  |& ts >> "$HOME"/logs/parse_new_s3."$DATE".log
 
-date; echo "parse GS"
+echo "parse GS"
 ./parse.sh GS      |& ts >> "$HOME"/parse_gs.log
 ./parse_new.sh GS  |& ts >> "$HOME"/logs/parse_new_gs."$DATE".log
 
-#date; echo "parse OP"
+#echo "parse OP"
 #./parse.sh OP      |& ts >> "$HOME"/parse_op.log
 #./parse_new.sh OP  |& ts >> "$HOME"/logs/parse_new_op."$DATE".log
 
-date; echo "parse Splunk"
+echo "parse Splunk"
 ./parse.sh Splunk   |& ts >> "$HOME"/parse_splunk.log
 ./parse_new.sh Splunk   |& ts >> "$HOME"/parse_new_splunk.log
 
-date; echo "s3_lister"
+echo "s3_lister"
 ./s3_lister.sh |& ts >> "$HOME"/s3_lister."$DATE".log
 ./mirror_obj.sh |& ts >> "$HOME"/mirror_obj."$DATE".log
 
 dow=$(date +%u)
 if [ "$dow" = "1" ] || [ "$dow" = "4" ]; then
-    date; echo "bigquery_export"
+    echo "bigquery_export"
     ./bigquery_cloudian.sh  |& ts >> "$HOME"/logs/bigquery_cloudian."$DATE".log
     ./bigquery_export.sh  |& ts >> "$HOME"/logs/bigquery_export."$DATE".log
     ./bigquery_report.sh  |& ts >> "$HOME"/logs/bigquery_report."$DATE".log
